6030e3a6a683c5acd9b63188335fd1c21a0b5d08
Michael Cavaliere <cavalier23@up.edu>
2019-11-19 19:26:55 -0800

    adding template for SendGrid

---
+++ js/emailcontact.js
@@ -0,0 +1,21 @@
+var apikey = 'https://personalwebsitekeyvault.vault.azure.net/secrets/emailapikey/';
+var apipath = 'http://'
+var email = 'email';
+var subject = 'subject';
+var message = 'message';
+
+function setup (){
+    loadJSON (apipath, gotDate);
+}
+
+function getDate (data){
+
+
+}
+
+var button = select('submit');
+button.mousePressed();
+
+
+
+
